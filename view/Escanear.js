e7da514466521721ba39c3ed02c03426a673146b
adrianojsilva2 <adriano.vurmo@gmail.com>
2020-12-18 01:02:11 -0300

    Erro na tentativa de inserir ML Kit

--- view/Escanear.js
+++ view/Escanear.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+//import {Text, View, BarcodeScannerOptions, Barcode} from 'react-native';
 
 export default function Escanear() {
   return (
@@ -8,3 +9,21 @@ export default function Escanear() {
     </View>
   );
 }
+
+/*
+const options = BarcodeScannerOptions.Builder()
+  .setBarcodeFormats(Barcode.FORMAT_QR_CODE, Barcode.FORMAT_EAN_13)
+  .build();
+*/
+
+/*
+ private class YourImageAnalyzer {ImageAnalysis;Analyzer} ({
+   function analyze(imageProxy = ImageProxy){
+        const mediaImage (imageProxy.image())
+        if (mediaImage != null) {
+            const image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            // Pass image to an ML Kit Vision API
+            // ...
+        }
+  }
+  */
--- view/Results.js
+++ view/Results.js
@@ -1,4 +1,3 @@
-import react from 'react';
 import React from 'react';
 import {Text, View} from 'react-native';
 
